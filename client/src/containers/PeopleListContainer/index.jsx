befe91849e73695b40877e70625603140f0340b4
Varas27 <ramivaras27@gmail.com>
2022-08-18 00:44:24 -0300

    Trying to make the front work on heroku

--- client/src/containers/PeopleListContainer/index.jsx
+++ client/src/containers/PeopleListContainer/index.jsx
@@ -2,7 +2,7 @@ import { PeopleList } from "../../components/PeopleList";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const apiUrl = process.env.NODE_ENV === 'development' ? `http://localhost:${process.env.PORT || 8080}` : 'https://random-people-varas.herokuapp.com'
+const apiUrl = 'https://random-people-varas.herokuapp.com'
 console.log(apiUrl)
 
 export const PeopleListContainer = () => {
